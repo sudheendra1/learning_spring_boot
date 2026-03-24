497a542c45a44f35806115ce159d9f8f95d2a93f
Sudheendra <sudheendra579@gmail.com>
2026-03-24 18:32:17 +0530

    Learnt Auto Wiring in spring boot

--- SpringBootdemo/src/main/java/com/sudheendra/SpringBootdemo/Alien.java
+++ SpringBootdemo/src/main/java/com/sudheendra/SpringBootdemo/Alien.java
@@ -1,10 +1,16 @@
 package com.sudheendra.SpringBootdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
+
+    @Autowired
+    Laptop laptop;
+
     public void  code(){
+        laptop.compile();
         System.out.println("Coding");
     }
 }
---
+++ SpringBootdemo/src/main/java/com/sudheendra/SpringBootdemo/Laptop.java
@@ -0,0 +1,11 @@
+package com.sudheendra.SpringBootdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void  compile(){
+        System.out.println("Compiling");
+    }
+}
