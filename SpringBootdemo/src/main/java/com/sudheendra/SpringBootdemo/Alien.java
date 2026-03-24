3471b38cb86ceaf3fe8f1ce44a07458d388bf5bc
Sudheendra <sudheendra579@gmail.com>
2026-03-24 18:22:28 +0530

    Learnt Dependency injection in spring boot

--- SpringBootdemo/src/main/java/com/sudheendra/SpringBootdemo/Alien.java
+++ SpringBootdemo/src/main/java/com/sudheendra/SpringBootdemo/Alien.java
@@ -1,4 +1,10 @@
 package com.sudheendra.SpringBootdemo;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Alien {
+    public void  code(){
+        System.out.println("Coding");
+    }
 }
--- SpringBootdemo/src/main/java/com/sudheendra/SpringBootdemo/SpringBootdemoApplication.java
+++ SpringBootdemo/src/main/java/com/sudheendra/SpringBootdemo/SpringBootdemoApplication.java
@@ -2,13 +2,22 @@ package com.sudheendra.SpringBootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootdemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootdemoApplication.class, args);
+//		SpringApplication.run(SpringBootdemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringBootdemoApplication.class, args);
 		System.out.println("Hello World");
+
+//		Alien obj = new Alien();
+
+		Alien obj = context.getBean(Alien.class);
+		obj.code();
+
+
 	}
 
 }
