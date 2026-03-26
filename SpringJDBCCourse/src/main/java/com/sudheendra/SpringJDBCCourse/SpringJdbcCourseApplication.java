6dbbd22f85ec03a7fc6aef2d2de553869de2bf47
Sudheendra <sudheendra579@gmail.com>
2026-03-26 15:16:20 +0530

    created springboot JDBC course project

---
+++ SpringJDBCCourse/src/main/java/com/sudheendra/SpringJDBCCourse/SpringJdbcCourseApplication.java
@@ -0,0 +1,13 @@
+package com.sudheendra.SpringJDBCCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcCourseApplication.class, args);
+	}
+
+}
---
+++ SpringJDBCCourse/src/test/java/com/sudheendra/SpringJDBCCourse/SpringJdbcCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudheendra.SpringJDBCCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
