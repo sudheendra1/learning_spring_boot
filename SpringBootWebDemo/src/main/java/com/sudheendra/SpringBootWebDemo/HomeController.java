2838a407e653661b30a912312c3d71e1a4461c82
Sudheendra <sudheendra579@gmail.com>
2026-03-30 00:24:48 +0530

    Created a request page and controller for it

--- SpringBootWebDemo/src/main/java/com/sudheendra/SpringBootWebDemo/HomeController.java
+++ SpringBootWebDemo/src/main/java/com/sudheendra/SpringBootWebDemo/HomeController.java
@@ -1,14 +1,23 @@
 package com.sudheendra.SpringBootWebDemo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    @RequestMapping("/")
     public String home(){
 
         System.out.println("Home Method Called");
         return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    public String add(){
+
+
+        return "result.jsp";
     }
 
 }
---
+++ SpringBootWebDemo/src/main/webapp/result.jsp
@@ -0,0 +1,13 @@
+<%@page language="java" %>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+</body>
+<h2>Result is: </h2>
+
+
+
+</body>
+</html>
