8aac6a8752ab9de6fca1679b903b7c5689fcfde4
Sudheendra <sudheendra579@gmail.com>
2026-03-29 22:48:05 +0530

    created servlet and sent response with mapping

--- servletdemo/src/main/java/com/sudheendra/App.java
+++ servletdemo/src/main/java/com/sudheendra/App.java
@@ -1,9 +1,25 @@
 package com.sudheendra;
 
-public class App 
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import java.io.File;
+
+public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws LifecycleException {
         System.out.println( "Hello World!" );
+
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.getConnector();
+
+
+        Context context = tomcat.addContext("",new File(".").getAbsolutePath());
+        Tomcat.addServlet(context,"HelloServlet", new HelloServlet());
+        context.addServletMappingDecoded("/hello","HelloServlet");
+
+        tomcat.start();
+        tomcat.getServer().await();
     }
 }
--- servletdemo/src/main/java/com/sudheendra/HelloServlet.java
+++ servletdemo/src/main/java/com/sudheendra/HelloServlet.java
@@ -1,4 +1,23 @@
 package com.sudheendra;
 
-public class HelloServlet {
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+    public  void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("In Service");
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<h2><b>Hello Servlet Working!</b></h2>");
+
+    }
+
+
 }
