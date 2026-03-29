f760526d23e8ff6ec5191f373a6913c5b37b2534
Sudheendra <sudheendra579@gmail.com>
2026-03-29 22:49:09 +0530

    created a spring boot project to learn about model view controller(MVC)

---
+++ SpringBootWebDemo/src/test/java/com/sudheendra/SpringBootWebDemo/SpringBootWebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudheendra.SpringBootWebDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
