d8b3baa2c0b38017cf2dd48eb59f371f82b05213
Sudheendra <sudheendra579@gmail.com>
2026-03-30 00:32:47 +0530

    added two numbers and displayed on page

--- SpringBootWebDemo/src/main/java/com/sudheendra/SpringBootWebDemo/HomeController.java
+++ SpringBootWebDemo/src/main/java/com/sudheendra/SpringBootWebDemo/HomeController.java
@@ -1,5 +1,7 @@
 package com.sudheendra.SpringBootWebDemo;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,7 +16,13 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(){
+    public String add(HttpServletRequest request, HttpSession session){
+
+        int num1 = Integer.parseInt(request.getParameter("num1"));
+        int num2 = Integer.parseInt(request.getParameter("num2"));
+        int result = num1+num2;
+
+        session.setAttribute("result",result);
 
 
         return "result.jsp";
--- SpringBootWebDemo/src/main/webapp/result.jsp
+++ SpringBootWebDemo/src/main/webapp/result.jsp
@@ -5,7 +5,7 @@
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
 </body>
-<h2>Result is: </h2>
+<h2>Result is: ${result}</h2>
 
 
 
