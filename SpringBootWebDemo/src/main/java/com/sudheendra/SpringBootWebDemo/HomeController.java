04569cfb6d3c41e4506b566d6c1fc91063bf2fd3
Sudheendra <sudheendra579@gmail.com>
2026-03-30 11:09:26 +0530

    Learned about requestparameters

--- SpringBootWebDemo/src/main/java/com/sudheendra/SpringBootWebDemo/HomeController.java
+++ SpringBootWebDemo/src/main/java/com/sudheendra/SpringBootWebDemo/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -15,11 +16,23 @@ public class HomeController {
         return "index.jsp";
     }
 
-    @RequestMapping("add")
-    public String add(HttpServletRequest request, HttpSession session){
+//    @RequestMapping("add")
+//    public String add(HttpServletRequest request, HttpSession session){
+//
+//        int num1 = Integer.parseInt(request.getParameter("num1"));
+//        int num2 = Integer.parseInt(request.getParameter("num2"));
+//        int result = num1+num2;
+//
+//        session.setAttribute("result",result);
+//
+//
+//        return "result.jsp";
+//    }
 
-        int num1 = Integer.parseInt(request.getParameter("num1"));
-        int num2 = Integer.parseInt(request.getParameter("num2"));
+    @RequestMapping("add")
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, HttpSession session){
+        //can use the request parameter when the variables name in the html code and method mismatch or else they will be autoassigned
+
         int result = num1+num2;
 
         session.setAttribute("result",result);
