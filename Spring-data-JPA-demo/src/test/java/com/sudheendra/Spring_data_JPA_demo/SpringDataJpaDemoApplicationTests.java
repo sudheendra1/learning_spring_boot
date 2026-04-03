0d7ed8d22bcfefb96ab1d4142791a198c307d749
Sudheendra <sudheendra579@gmail.com>
2026-04-03 12:01:28 +0530

    Created project for Spring JPA Learning

---
+++ Spring-data-JPA-demo/src/test/java/com/sudheendra/Spring_data_JPA_demo/SpringDataJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudheendra.Spring_data_JPA_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
